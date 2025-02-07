60be986db502a0f1d47991b342c77c3f66791139
Moris Afram <moris.afram@learnet.se>
2025-02-07 13:56:33 +0100

    add and changes in yaml

--- MyencryptAPI/Program.cs
+++ MyencryptAPI/Program.cs
@@ -1,5 +1,3 @@
-using Encryptolibrary;
-
 
 namespace MyencryptAPI
 {
@@ -14,23 +12,59 @@ namespace MyencryptAPI
             "/encrypt?input= \n" +
             "/decrypt?input=  ");
 
-            app.MapGet("/encrypt", (string input) => Encrypt(input));
+            app.MapGet("/encrypt", (string input) => Encrypto(input));
             app.MapGet("/decrypt", (string input) => Decrypt(input));
             app.Run();
 
-            string Encrypt(string input)
+            string Encrypto(string input)
             {
-                Enkrypto encrypt = new Enkrypto();
-                string encrypted = encrypt.Encrypt(input, 3);
+                string encrypted = Encrypt(input, 3);
                 return $"Encrypt:  {encrypted}  ";
             }
 
             string Decrypt(string input)
             {
-                Enkrypto decrypt = new Enkrypto();
-                string decrypted = decrypt.Decrypt(input, 3);
+                string decrypted = Encrypt(input, -3);
                 return $"Decrypt:  {decrypted}  ";
             }
         }
+
+        public static string Encrypt(string input, int shift)
+        {
+            string result = "";
+
+            foreach (char letter in input)
+            {
+                if (char.IsLetter(letter))
+                {
+                    if (char.IsLower(letter))
+                    {
+                        char shifted = (char)((letter - 'a' + shift) % 26 + 'a');
+                        result += shifted;
+                    }
+                    else if (char.IsUpper(letter))
+                    {
+                        char shifted = (char)((letter - 'A' + shift) % 26 + 'A');
+                        result += shifted;
+                    }
+                }
+                else if (char.IsDigit(letter))
+                {
+                    char shifted = (char)((letter - '0' + shift) % 10 + '0');
+                    result += shifted;
+                }
+                else
+                {
+                    result += letter;
+                }
+            }
+            return result;
+        }
+
+        public static string Decrypt(string input, int shift)
+        {
+            return Encrypt(input, -shift);
+        }
     }
 }
+
