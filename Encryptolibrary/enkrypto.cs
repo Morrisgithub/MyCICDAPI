72d175eb2bb348596d1c4c4ddeebb9bf3bfe815d
Moris Afram <moris.afram@learnet.se>
2025-02-07 11:40:28 +0100

    add library code

--- Encryptolibrary/enkrypto.cs
+++ Encryptolibrary/enkrypto.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace Encryptolibrary
 {
-    internal class enkrypto
+    public class enkrypto
     {
+        public string Encrypt(string input, int shift)
+        {
+            string result = "";
+
+            foreach (char letter in input)
+            {
+                if (char.IsLetter(letter))
+                {
+                    
+                    if (char.IsLower(letter))
+                    {
+                        
+                        char shifted = (char)((letter - 'a' + shift) % 26 + 'a');
+                        result += shifted;
+                    }
+                    
+                    else if (char.IsUpper(letter))
+                    {
+                        char shifted = (char)((letter - 'A' + shift) % 26 + 'A');
+                        result += shifted;
+                    }
+                }
+                
+                else if (char.IsDigit(letter))
+                {
+                    char shifted = (char)((letter - '0' + shift) % 10 + '0');
+                    result += shifted;
+                }
+                else
+                {
+                    
+                    result += letter;
+                }
+
+            }
+            return result;
+        }
+
+        public string Decrypt(string input, int shift)
+        {
+            return Encrypt(input, -shift);
+        }
     }
+}
 }
